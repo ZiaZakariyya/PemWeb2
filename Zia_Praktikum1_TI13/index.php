1e3a28b52d5aec56d6eb987bc2d3a5e421d7982d
DhiaYa <114472353+DhiaYa@users.noreply.github.com>
2023-02-26 09:06:56 +0700

    Add files via upload

---
+++ Zia_Praktikum1_TI13/index.php
@@ -0,0 +1,9 @@
+<?php
+
+    $nama = "Zia Zakariyya Al Ghaniy";
+    $umur = "19";
+    $berat = "75";
+
+    echo "Nama saya : " .         $nama;
+    echo "<br>Umur saya : " .         $umur;
+    echo "<br>Berat saya : " .        $berat;
